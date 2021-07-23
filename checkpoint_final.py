b93cee17c08ad7c9dab6c1404f7087bd51735365
Sebastien Dupont <79512309+Seb-Dupont-DataAnalyst@users.noreply.github.com>
2021-07-23 16:47:02 +0200

    Update checkpoint_final.py

--- checkpoint_final.py
+++ checkpoint_final.py
@@ -976,8 +976,9 @@ if choice == 'Pricing via Machine Learning' :
             fig.update_layout({'plot_bgcolor': 'rgba(255,255,255,255)',
                                         'paper_bgcolor': 'rgba(255,255,255,255)', })
             fig.update_traces(texttemplate='%{text:.3s}', textposition='outside')
-            fig.update_xaxes(title='Prix', tick_prefix = '$')
+            fig.update_xaxes(title='Prix')
             fig.update_yaxes(title='Vins')
+            fig.update_layout(xaxis_tickprefix = '$')
             st.plotly_chart(fig, use_container_width=True)    
 
         if choix_hypothese == 'Hypothèse haute':
